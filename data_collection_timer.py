5c48684fdad86c095bc3d6289fbefe736a85072a
raktimhalder241 <raktimhalder241@gmail.com>
2020-10-10 01:45:06 +0530

    Added files necessary to record data
    Added files
    1. data_collection_timer.py
    2. data_collector.py
    3. insert_data.py
    4. create_database.py
    5. create_table.py

---
+++ data_collection_timer.py
@@ -0,0 +1,14 @@
+import os
+from datetime import datetime
+
+old_time = ""
+current_time = ""
+
+while(1) :
+  now = datetime.now()
+  #current_time = now.strftime("%H:%M:%S")
+  current_time = now.strftime("%M")
+  if current_time != old_time :
+    if int(current_time) % 15 == 0 :
+      os.system("python3 data_collector.py")
+      old_time = current_time
