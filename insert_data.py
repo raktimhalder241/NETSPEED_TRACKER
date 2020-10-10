f7f886de77bc8651e065e21e47592c4e17162281
raktimhalder241 <raktimhalder241@gmail.com>
2020-10-10 23:45:44 +0530

    insert_data.py
    updated insert_data.py

--- insert_data.py
+++ insert_data.py
@@ -11,8 +11,8 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO netspeed_data (download, upload, ping, time) VALUES (%s, %s, %s, %s)"
-val = (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+sql = "INSERT INTO netspeed_data (download, upload, ping, time, date) VALUES (%s, %s, %s, %s, %s)"
+val = (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
 mycursor.execute(sql, val)
 
 mydb.commit()
